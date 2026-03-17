bfb35aca34fb387ae863ba547037a52b51d746b0
yossi_16 <ylerer770@gmail.com>
2026-03-17 16:13:20 +0200

    add create user in register

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,10 +10,21 @@ export default function NavBar() {
     function toadd(){
         navigate('/Add-Launcher')
     }
+    function toRegister(){
+        navigate('/register')
+    }
+    function toLogOut(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        navigate('/')
+
+    }
   return (
     <div className='navbar' style={{width:'100%', backgroundColor:"gray", height:'40px', display: 'flex', justifyContent: 'center', alignItems:'center', marginBottom:'30px'}}>
         <button onClick={toHome} style={{margin: '10px'}}>Home</button>
         <button onClick={toadd} style={{margin: '10px'}}>Add Launcher</button>
+        <button onClick={toRegister}>Register</button>
+        {localStorage.getItem('user') && <button onClick={toLogOut}>Log Out</button>}
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,75 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Register() {
+
+    const [username, setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [userType, setUserType] = useState('')
+
+    const token = localStorage.getItem('token')
+
+    async function createUser(){
+        try{
+            const respons = await fetch('http://localhost:3000/api/auth/register/create',{
+                method: 'post',
+                headers: {
+                    "Content-type": "application/json",
+                    authorization: 'Bearer ' + token,
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    email,
+                    type_user: userType
+                }),
+            });
+            console.log(respons);
+            
+            if(respons.ok){
+                const result = await respons.json()
+                alert(result.id)
+                setUsername('')
+                setPassword('')
+                setEmail('')
+                setUserType('')
+            }
+            
+        } catch(e){
+            alert(e.message)
+        }
+    }
+
+
   return (
-    <div>Register</div>
+    <div>
+        <h1>register:</h1>
+        <div>
+            <h2>create new user:</h2>
+            <div>
+                <label>user name: </label>
+                <input type="text" onChange={e => setUsername(e.target.value)} value={username}/>
+            </div>
+
+            <div>
+                <label>password: </label>
+                <input type="text" onChange={e => setPassword(e.target.value)} value={password}/>
+            </div>
+
+            <div>
+                <label>email: </label>
+                <input type="text" onChange={e => setEmail(e.target.value)} value={email}/>
+            </div>
+
+            <div>
+                <label>user type: </label>
+                <input type="text" onChange={e => setUserType(e.target.value)} value={userType}/>
+            </div>
+            <div>
+                <button onClick={createUser}>create</button>
+            </div>
+        </div>
+    </div>
   )
 }
